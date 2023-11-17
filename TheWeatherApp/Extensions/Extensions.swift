39a073b1772ae065e9bb6559457d7a07c695ac71
anzmax <anzmaksimovic@gmail.com>
2023-11-17 17:25:15 +0300

    improved services and added settings controller

--- TheWeatherApp/Extensions/Extensions.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-extension UIColor {
-    static let customBlue = UIColor(red: 0.12, green: 0.3, blue: 0.77, alpha: 1)
-    static let customOrange = UIColor(red: 0.949, green: 0.431, blue: 0.067, alpha: 1)
-}
